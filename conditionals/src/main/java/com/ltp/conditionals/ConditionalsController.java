18af55a60971482085e469c61f61bc578374440d
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-24 22:32:32 -0600

    tested thymeleaf

--- conditionals/src/main/java/com/ltp/conditionals/ConditionalsController.java
+++ conditionals/src/main/java/com/ltp/conditionals/ConditionalsController.java
@@ -2,13 +2,14 @@ package com.ltp.conditionals;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.ui.Model;
 
 @Controller
 public class ConditionalsController {
 
     @GetMapping(value="/")
-    public String getMethodName() {
+    public String getMethodName(Model model) {
+        model.addAttribute("sales", 100);
         return "conditionals";
     }
     
