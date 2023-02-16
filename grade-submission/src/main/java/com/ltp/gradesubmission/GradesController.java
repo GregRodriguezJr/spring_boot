1bc7a57a54797c6607f08a164535fb4cefc414a9
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-02-16 13:28:13 -0600

    added array list to render grades

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradesController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradesController.java
@@ -4,11 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class GradesController {
 
+    List <Grade> studentGrades = Arrays.asList(
+            new Grade("Greg", "Java", "A"),
+            new Grade("John", "Java", "C"),
+            new Grade("Bill", "Java", "B")
+    );
+
     @GetMapping("/grades")
     public String getGrades(Model model) {
+
+        model.addAttribute("grades", studentGrades);
+
         Grade grade = new Grade("Greg", "Java", "A");
         model.addAttribute("grade", grade);
         return "grades";
