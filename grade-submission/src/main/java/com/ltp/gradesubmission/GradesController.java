a2548f763dd6db3fb90b76dbfa467e9a76791814
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-02-16 22:08:25 -0600

    created post mapping for grade submission form

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradesController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradesController.java
@@ -3,19 +3,15 @@ package com.ltp.gradesubmission;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class GradesController {
 
-    List <Grade> studentGrades = Arrays.asList(
-            new Grade("Greg", "Java", "A"),
-            new Grade("John", "Java", "C"),
-            new Grade("Bill", "Java", "B")
-    );
+    List <Grade> studentGrades = new ArrayList<>();
 
     @GetMapping("/")
     public String gradeForm(Model model) {
@@ -25,12 +21,14 @@ public class GradesController {
 
     @GetMapping("/grades")
     public String getGrades(Model model) {
-
         model.addAttribute("grades", studentGrades);
-
-        Grade grade = new Grade("Greg", "Java", "A");
-        model.addAttribute("grade", grade);
         return "grades";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade) {
+        studentGrades.add(grade);
+        return "redirect:/grades";
     }
 
 }
