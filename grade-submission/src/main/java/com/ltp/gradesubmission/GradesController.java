29cf4de9dd34a577eb42aa3ab3254cb1e433cdd2
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-02-18 13:35:24 -0600

    added edit button, created getGradeIndex method

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradesController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradesController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +15,7 @@ public class GradesController {
     List <Grade> studentGrades = new ArrayList<>();
 
     @GetMapping("/")
-    public String gradeForm(Model model) {
+    public String gradeForm(Model model, @RequestParam(required = false) String name) {
         model.addAttribute("grade", new Grade());
         return "form";
     }
@@ -23,6 +24,15 @@ public class GradesController {
     public String getGrades(Model model) {
         model.addAttribute("grades", studentGrades);
         return "grades";
+    }
+
+    public Integer getGradeIndex(String name) {
+        for (int i = 0; i < studentGrades.size(); i++) {
+            if (studentGrades.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @PostMapping("/handleSubmit")
